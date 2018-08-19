14cf0ffdc0b70e8d0e2cc66aaf6ac8e27e4d5cf5
bytecore@gmail.com <mo10128>
2018-08-18 23:58:49 -0300

    Criando a classe Usuario

--- app/src/main/java/br/com/mojumob/crudfirebase/activity/MainActivity.java
+++ app/src/main/java/br/com/mojumob/crudfirebase/activity/MainActivity.java
@@ -2,9 +2,12 @@ package br.com.mojumob.crudfirebase.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import br.com.mojumob.crudfirebase.R;
 
@@ -16,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnLogin;
     private TextView txtCriarConta;
     private TextView txtEsqueceuSenha;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +27,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Inicializações
-        edtLogin = findViewById(R.id.edtLogin);
-        edtSenha = findViewById(R.id.edtSenha);
+        edtLogin         = findViewById(R.id.edtLogin);
+        edtSenha         = findViewById(R.id.edtSenha);
+        btnLogin         = findViewById(R.id.btnLogin);
+        txtCriarConta    = findViewById(R.id.txtCriarConta);
+        txtEsqueceuSenha = findViewById(R.id.txtEsqueceuSenha);
+
+        //Ação de clique no botao login
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
