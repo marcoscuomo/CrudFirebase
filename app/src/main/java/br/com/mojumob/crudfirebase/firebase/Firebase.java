215eacf909170db3077cf01209e102dedd8d3568
bytecore@gmail.com <mo10128>
2018-08-18 23:51:51 -0300

    Criando a classe de auxilio para uso do Firebase

--- app/src/main/java/br/com/mojumob/crudfirebase/firebase/Firebase.java
+++ app/src/main/java/br/com/mojumob/crudfirebase/firebase/Firebase.java
@@ -1,4 +1,28 @@
 package br.com.mojumob.crudfirebase.firebase;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Firebase {
+
+    private static FirebaseAuth autenticacao;
+    private static DatabaseReference databaseReference;
+
+    private static DatabaseReference getFirebaseDatabse(){
+
+        if(databaseReference == null){
+            databaseReference = FirebaseDatabase.getInstance().getReference();
+        }
+        return databaseReference;
+    }
+
+    private static FirebaseAuth getFirebaseAutenticacao(){
+        if(autenticacao == null){
+             autenticacao = FirebaseAuth.getInstance();
+        }
+
+        return autenticacao;
+    }
+
 }
