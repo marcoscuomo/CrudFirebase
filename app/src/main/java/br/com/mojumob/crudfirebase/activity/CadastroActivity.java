25e68e6aaccddaa84a60abc325864e1c98d11c7c
bytecore@gmail.com <mo10128>
2018-08-19 10:48:11 -0300

    Criando o metodo de savar no firebase

--- app/src/main/java/br/com/mojumob/crudfirebase/activity/CadastroActivity.java
+++ app/src/main/java/br/com/mojumob/crudfirebase/activity/CadastroActivity.java
@@ -1,5 +1,6 @@
 package br.com.mojumob.crudfirebase.activity;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,13 +8,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+
 import br.com.mojumob.crudfirebase.R;
+import br.com.mojumob.crudfirebase.firebase.Firebase;
+import br.com.mojumob.crudfirebase.model.Usuario;
 
 public class CadastroActivity extends AppCompatActivity {
 
     //Atributos
     private EditText edtNome, edtEmail, edtSenha;
     private Button btnCadastrar;
+    private Usuario usuario;
+    private FirebaseAuth autenticacao;
 
 
     @Override
@@ -42,6 +55,9 @@ public class CadastroActivity extends AppCompatActivity {
                     if(!email.isEmpty()){
                         if(!senha.isEmpty()){
 
+                            usuario = new Usuario(nome, email, senha);
+                            cadastrarUsuario(usuario);
+
                         }else{
                             exibiMensagemCampoVazio("Senha");
                         }
@@ -58,6 +74,48 @@ public class CadastroActivity extends AppCompatActivity {
         });//Fim do evento click botao cadadastrar
     }
 
+    //Metodo para cadastrar o usuario após a checagem de que não há campo vazio
+    private void cadastrarUsuario(Usuario usuario) {
+
+        autenticacao = Firebase.getFirebaseAutenticacao();
+        autenticacao.createUserWithEmailAndPassword(usuario.getEmail(), usuario.getSenha())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+
+                            Toast.makeText(CadastroActivity.this, "Cadastro criado com sucesso", Toast.LENGTH_SHORT).show();
+
+                        }else{
+                            //Tratamento de excesao ao criar uma conta
+                            String excesao = "";
+                            try{
+                                throw task.getException();
+                            }catch (FirebaseAuthWeakPasswordException e){
+                                excesao = "Digite uma senha mais forte";
+                            }catch (FirebaseAuthInvalidCredentialsException e){
+                                excesao = "Digite um E-mail válido";
+                            }catch (FirebaseAuthUserCollisionException e){
+                                excesao = "E-mail já cadastrado";
+                            }
+                            catch (Exception e) {
+                                excesao = "Erro ca cadastrar o usuário " + e.getMessage();
+                                e.printStackTrace();
+                            }
+
+                            Toast.makeText(CadastroActivity.this,
+                                    excesao, Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });//Fim do onCompleteListenet
+
+
+    }//Fim do metodo cadastrarUsuario
+
+
+    //Metodo para exibir um toast em caso de campo vazio
     private void exibiMensagemCampoVazio(String campo) {
         Toast.makeText(this, "Por favor, preencha o campo " + campo +
                 " para concluir o cadastro", Toast.LENGTH_LONG).show();
