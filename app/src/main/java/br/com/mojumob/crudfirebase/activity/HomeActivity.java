6306c6ecb6ee80be159686e702a5837499bf8ec9
bytecore@gmail.com <mo10128>
2018-08-19 11:24:58 -0300

    Criando o metodo de logout

--- app/src/main/java/br/com/mojumob/crudfirebase/activity/HomeActivity.java
+++ app/src/main/java/br/com/mojumob/crudfirebase/activity/HomeActivity.java
@@ -1,15 +1,25 @@
 package br.com.mojumob.crudfirebase.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import br.com.mojumob.crudfirebase.R;
+import br.com.mojumob.crudfirebase.firebase.Firebase;
+
 
 public class HomeActivity extends AppCompatActivity {
+
+    //Atributos
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,4 +38,25 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_sair, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.menu.menu_sair:
+                autenticacao = Firebase.getFirebaseAutenticacao();
+                autenticacao.signOut();
+                startActivity(new Intent(HomeActivity.this, MainActivity.class));
+                finish();
+                break;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
 }
