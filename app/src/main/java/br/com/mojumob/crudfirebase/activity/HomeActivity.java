056dda818e26bb0c9418fd483e3ba23ea35c029e
bytecore@gmail.com <mo10128>
2018-08-19 13:13:40 -0300

    Criando o alertDialog no Float Action button para cadastar contatos

--- app/src/main/java/br/com/mojumob/crudfirebase/activity/HomeActivity.java
+++ app/src/main/java/br/com/mojumob/crudfirebase/activity/HomeActivity.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
@@ -30,9 +34,9 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         //Inicializações
-        Toolbar toolbar          = findViewById(R.id.toolbar);
+        Toolbar toolbar          = findViewById(R.id.toolbarMain);
         FloatingActionButton fab = findViewById(R.id.fab);
-        //searchView               = findViewById(R.id.pesquisarPrincipal);
+        searchView               = findViewById(R.id.persquisarPrincipal);
 
         toolbar.setTitle("Lista de contatos");
         setSupportActionBar(toolbar);
@@ -41,8 +45,30 @@ public class HomeActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Iniciando o Alterdialog
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(HomeActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.dialog_cadastrar, null);
+
+                //Iniciando os elementos do xml
+                final EditText edtNome           = mView.findViewById(R.id.dialog_edtNome);
+                EditText edtEmail          = mView.findViewById(R.id.dialog_edtEmail);
+                EditText edtTelefone       = mView.findViewById(R.id.dialog_edtTelefone);
+                Button btnCadastrarContato = mView.findViewById(R.id.dialog_btnCadastrar);
+
+                //Tratando a açao de clique
+                btnCadastrarContato.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(!edtNome.getText().toString().isEmpty()){
+                            Toast.makeText(HomeActivity.this, R.string.ok_clicado, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                mBuilder.setView(mView);
+                AlertDialog dialog = mBuilder.create();
+                dialog.show();
+
             }
         });
     }
